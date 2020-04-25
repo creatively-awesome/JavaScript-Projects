ef892bad33b2c4d5c2c12b470c74e880de014419
creatively-awesome <46718812+creatively-awesome@users.noreply.github.com>
2020-04-25 19:40:42 +0530

    Create rock-paper-scissors.js

---
+++ rock-paper-scissors.js
@@ -0,0 +1,68 @@
+const getUserChoice = userInput => {
+  userInput = userInput.toLowerCase();
+  if(userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'){
+    return userInput;
+  }
+  else{
+    console.log('You really need to learn this game!');
+  }
+}
+
+const getComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * 3);
+  switch(randomNumber){
+    case 0:
+      return 'rock';
+      break;
+    case 1:
+      return 'paper';
+      break;
+    case 2:
+      return 'scissors';
+      break;
+    default:
+      return 'Thinking';
+      break;
+  }
+}
+const determineWinner = (userChoice, computerChoice) => {
+  if(userChoice === computerChoice){
+    return 'Guess you were lucky! The game was a tie.'
+  }
+  if(userChoice === 'rock'){
+    if(computerChoice === 'scissors'){
+      return 'You won!';
+    }
+    else{
+      return 'Guess who just had his ass kicked? You. HAHAHA!';
+    }
+  }
+  if(userChoice === 'paper'){
+    if(computerChoice === 'rock'){
+      return 'You won. Guess today is the looser\'s day!';
+    }
+    else{
+      return 'Guess who just had his ass kicked? You. HAHAHA!';
+    }
+  }
+  if(userChoice === 'scissors'){
+    if(computerChoice === 'paper'){
+      return 'You won. Guess today is the looser\'s day!';
+    }
+    else{
+      return 'Guess who just had his ass kicked? You. HAHAHA!';
+    }
+  }
+}
+
+function playGame(){
+  const userChoice = getUserChoice('rock');
+  const computerChoice = getComputerChoice();
+  
+  console.log('Your threw: ' +userChoice);
+  console.log('Computer threw: ' +computerChoice);
+  
+  console.log(determineWinner(userChoice, computerChoice));  
+}
+
+playGame();
